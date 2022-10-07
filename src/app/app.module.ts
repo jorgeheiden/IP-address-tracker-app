f8b508e142a1f35e8cb417983644d14347bf9802
jorge heidenreich <jmjhjorge@gmail.com>
2022-10-07 13:02:44 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { DatosComponent } from './componentes/datos/datos.component';
+import { MapaComponent } from './componentes/mapa/mapa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DatosComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
