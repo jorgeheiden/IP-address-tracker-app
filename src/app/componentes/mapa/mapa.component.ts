e8cbf38044f90581ae86fe4b167d629610dbfc34
jorge heidenreich <jmjhjorge@gmail.com>
2022-10-08 16:33:04 -0300

    mapa añadido y funcionando

--- src/app/componentes/mapa/mapa.component.ts
+++ src/app/componentes/mapa/mapa.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Map, tileLayer } from 'leaflet'
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  
   constructor() { }
+  
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  
+  ngAfterViewInit(): void{
+    const map = new Map('map').setView([51.505, -0.09], 13);
+
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+}
 
 }
